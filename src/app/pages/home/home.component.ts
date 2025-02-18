d70f37f996c728a0c13dc991252adde7c9e4fe40
Alexmasi <alexmasi624@gmail.com>
2025-02-18 23:43:24 +0100

    Actualización del proyecto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CompoundInterestCalculatorComponent } from '../../components/compound-interest-calculator/compound-interest-calculator.component';  // 👈 Importación correcta
+import { CompoundInterestCalculatorComponent } from '../../components/compound-interest-calculator/compound-interest-calculator.component';  
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CompoundInterestCalculatorComponent], // 👈 Agregamos la calculadora
+  imports: [CommonModule, CompoundInterestCalculatorComponent], 
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   cryptos: any[] = [];
+  contador: number = 0;
   private API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
   ngOnInit() {
@@ -18,5 +19,14 @@ export class HomeComponent implements OnInit {
       .then(response => response.json())
       .then(data => this.cryptos = data)
       .catch(error => console.error('Error al cargar criptomonedas:', error));
+  }
+
+  incrementarContador() {
+    this.contador++;
+  }
+
+  decrementarContador(event: MouseEvent) {
+    event.preventDefault(); // Evita el menú contextual al hacer clic derecho
+    this.contador--;
   }
 }
